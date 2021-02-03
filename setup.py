e8ad5073fe16fda5ae796c27c33602115117a526
Josue87 <j.encinar.87@gmail.com>
2021-02-03 21:12:26 +0100

    update stup

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@
 from setuptools import setup, find_packages
 import metafinder
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
 
 setup(
     name='metafinder',
     version=metafinder.__version__,
     author='Josue Encinar (@JosueEncinar)',
     description='MetaFinder - Metadata search through Google',
-    long_description=long_description,
     include_package_data=True,
     license='GNU GPLv3+',
     packages=find_packages(),
